622648b1f35c2babbf867b48b967e2e2894d0654
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-17 15:23:07 -0300

    chore: add ThemeProvider, GlobalStyles, Header and BrowseRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from './assets/styles/themes';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router';
+import { GlobalStyles } from './assets/styles/global';
+import Header from './components/Header';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+          <GlobalStyles/>
+          <Header />
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
