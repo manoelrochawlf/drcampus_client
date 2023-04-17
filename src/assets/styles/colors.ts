151f9c82b86a8e0e648668e38b84ec8476346f9b
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-17 15:19:46 -0300

    chore: adding styled-components configuration

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primaryColor: '#f3474a',
+    secondaryColor: '#d82231',
+    terciaryColor: '#feebd0'
+}
---
+++ src/assets/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;  
+  }
+  body{
+   background: linear-gradient(76deg, rgba(252,248,248,1) 65%, rgba(254,235,208,1) 75%);
+  }
+  `
